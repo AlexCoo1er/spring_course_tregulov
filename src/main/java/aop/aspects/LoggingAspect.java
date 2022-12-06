f8802c4d6727cc0a2babcdcdc3419c13df8686c9
AlexCoo1er <118539001+AlexCoo1er@users.noreply.github.com>
2022-12-06 15:31:57 +0300

    Initial commit

---
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -0,0 +1,9 @@
+package aop.aspects;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+}
